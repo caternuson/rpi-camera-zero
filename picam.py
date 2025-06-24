79235aa9da7896ccd97ee09e2b384b4a35c18dad
Carter Nelson <caternuson@gmail.com>
2025-06-24 15:34:36 -0700

    updates

--- picam.py
+++ picam.py
@@ -1,17 +1,31 @@
+import time
 import os
 import json
 import asyncio
 import tornado
+from picamera2 import Picamera2
+
+picam2 = Picamera2()
+config = picam2.create_still_configuration( {"size":(1920, 1080)} )
+picam2.configure(config)
+picam2.start()
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("picam.html")
 
-class AjaxHandler(tornado.web.RequestHandler):
+    def get(self):
+        time_lapse_running = self.application.settings.get("time_lapse_running", False)
+        if time_lapse_running:
+            self.render("status.html")
+            #print("status")
+        else:
+            self.render("config.html")
+            #self.application.settings["time_lapse_running"] = False
+            #print("config")
+
     def post(self):
         resp = {"ERR":0}
         data = json.loads(self.request.body)
-        print(data)
+        #print(data)
         cmd = data.get("CMD", None)
         if cmd is None:
             resp["ERR"] = 1
@@ -23,6 +37,22 @@ class AjaxHandler(tornado.web.RequestHandler):
         elif cmd == "XPV":
             #stop preview
             pass
+        elif cmd == "STA":
+            # start time lapse
+            self.application.settings["time_lapse_running"] = True
+        elif cmd == "STO":
+            # stop time lapse
+            self.application.settings["time_lapse_running"] = False
+        elif cmd == "TIM":
+            # return system time
+            resp["TIME"] = time.time()
+        elif cmd == "TAK":
+            # take an image
+            filename = "static/preview.jpg"
+            url = "{}?{}".format(filename, time.time()) # prevent using cached image
+            settings = picam2.capture_file(filename)
+            resp["URL"] = url
+            resp["SET"] = settings
         else:
             resp["ERR"] = 2
         self.write(json.dumps(resp))
@@ -30,14 +60,15 @@ class AjaxHandler(tornado.web.RequestHandler):
 async def main():
     handlers = [
         (r"/", MainHandler),
-        (r"/ajax", AjaxHandler),
     ]
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static"),
         "template_path": os.path.join(os.path.dirname(__file__), "static"),
+        "time_lapse_running": False,
     }
     app = tornado.web.Application(handlers, **settings)
     app.listen(8888)
+    print("Server started.")
     shutdown_event = asyncio.Event()
     await shutdown_event.wait()
 
